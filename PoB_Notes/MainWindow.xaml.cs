5ce6dff7d5eb1e8fb0087dc3c2680e60b585271f
Бахтияр Башаров <bahtiyarbasharov@gmail.com>
2024-07-24 19:49:25 +0500

    Собрал статичное окно заметок. Выставлено ограничение по высоте и ширине. Цвета взяты с оригинальной программы.

--- PoB_Notes/MainWindow.xaml.cs
+++ PoB_Notes/MainWindow.xaml.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using PoB_Notes.View;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -23,6 +24,18 @@ namespace PoB_Notes
         public MainWindow()
         {
             InitializeComponent();
+            Loading();
+        }
+
+        private void Loading()
+        {
+            MainDescription _mainDescription = new MainDescription() 
+            { DescriptionText = 
+            $"Вы можете использовать комбинацию клавиш Ctrl +/- для увеличения/уменьшения размера текста.\n" +
+            $"Текстовые поля поддерживают подсветку кнопками ниже." +
+            $"Используйте их для описания, это поможет легче читать ваши заметки." };
+            this.DataContext = _mainDescription;
+
         }
     }
 }
---
+++ PoB_Notes/View/MainDescription.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PoB_Notes.View
+{
+    internal class MainDescription
+    {
+        public string DescriptionText { get; set; }
+
+        public MainDescription()
+        {
+            
+        }
+    }
+}
