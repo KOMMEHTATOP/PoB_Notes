5c3e3810a78f69426fb17a0136599cbbb24c5407
Бахтияр Башаров <bahtiyarbasharov@gmail.com>
2024-07-24 21:07:25 +0500

    Верстку сделал не правильно. Нужно сначала сделать базовые классы (как база данных) для управления параметрами типа цвета.

---
+++ PoB_Notes/DataBase/Strength.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Media;
+
+namespace PoB_Notes.DataBase
+{
+    internal class Strength
+    {
+        public Color StrengthColor { get; set; }
+        public int StrengthLevel { get; set; }
+        public double LifeUp { get; set; }
+        public double DamageUp { get; set; }
+        public string StrengthView { get; set; }
+    }
+}
